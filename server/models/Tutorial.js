737aa707e1fac10459e14b4d83ab3094612c6dc0
limkl.ryan@gmail.com <limkl.ryan@gmail.com>
2024-05-05 00:09:16 +0800

    formatting

--- server/models/Tutorial.js
+++ server/models/Tutorial.js
@@ -1,15 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
-    const Tutorial = sequelize.define("Tutorial", {
-        title: {
-            type: DataTypes.STRING(100),
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        }
-    }, {
-        tableName: 'tutorials'
-    });
-    return Tutorial;
-}
+  const Tutorial = sequelize.define(
+    "Tutorial",
+    {
+      title: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "tutorials",
+    }
+  );
+  return Tutorial;
+};
